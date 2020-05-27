55d3529fd31963826f86d6eadfbd46430f748a6b
Mickael Metesreau <mickael.metesreau@gmail.com>
2020-05-27 14:22:08 +0200

    init

---
+++ 11.ESGI.DesignPattern.Refactor/Item.cs
@@ -0,0 +1,13 @@
+﻿namespace _11.ESGI.DesignPattern.Refactor
+{
+    public class Item
+    {
+        public string Name { get; set; }
+        public int SellIn { get; set; }
+        public int Quality { get; set; }
+    }
+}
+
+
+
+
